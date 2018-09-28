8f4711590cb4a97f800ea42f3ce394e903028c51
zephyrzambrano <43213451+zephyrzambrano@users.noreply.github.com>
2018-09-28 16:13:34 -0400

    Showing When Slingshot Is Active

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour {
+
+    public GameObject launchPoint;
+
+    void Awake() {
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+    }
+
+    void OnMouseEnter() {
+        //print("Slingshot:OnMouseEnter()");
+        launchPoint.SetActive(true);
+    }
+
+    void OnMouseExit() {
+        //print("Slingshot:OnMouseExit()");
+        launchPoint.SetActive(false);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
